25e24e3d8fe1b6456c0dad5c3b5eff4d1bc9941f
Veselin Aleksandrov <v.aleksandrov91@gmail.com>
2017-05-21 17:59:03 +0300

    Added entity for address and write to it in Oracle XE DB

---
+++ src/main/java/bg/tu/masters/entity/AddressEntity.java
@@ -0,0 +1,80 @@
+package bg.tu.masters.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ADDRESS")
+public class AddressEntity implements Serializable {
+    private static final long serialVersionUID = -5561315426934538089L;
+
+    @Id
+    @Column(name="ID", nullable=false)
+    @SequenceGenerator(name="seq_address", sequenceName="seq_address_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE ,generator="seq_address")
+    private Long id;
+
+    @Column(name = "COUNTRY")
+    private String country;
+
+    @Column(name = "CITY")
+    private String city;
+
+    @Column(name = "ADDRESS1")
+    private String address1;
+
+    @Column(name = "ADDRESS2")
+    private String address2;
+
+    public AddressEntity() {
+        // Default public constructor.
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+}
--- src/main/java/bg/tu/masters/service/impl/AuthorizationServiceImpl.java
+++ src/main/java/bg/tu/masters/service/impl/AuthorizationServiceImpl.java
@@ -1,9 +1,13 @@
 package bg.tu.masters.service.impl;
 
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import bg.tu.masters.entity.AddressEntity;
 import bg.tu.masters.service.AuthorizationService;
 
 @Stateless
@@ -11,7 +15,18 @@ public class AuthorizationServiceImpl implements AuthorizationService {
 
     @Override
     public Response processAuthorizationRequest() {
-        return Response.status(Response.Status.OK).entity("OK").type(MediaType.TEXT_PLAIN).build();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("myOracle");
+        EntityManager em = emf.createEntityManager();
+
+        AddressEntity address = new AddressEntity();
+        address.setAddress1("MyAddress 1");
+        address.setAddress2("MyAddress 2");
+        address.setCity("MyCity");
+        address.setCountry("MyCountry");
+        em.persist(address);
+        em.flush();
+
+        return Response.status(Response.Status.CREATED).type(MediaType.APPLICATION_JSON).build();
     }
 
 }
